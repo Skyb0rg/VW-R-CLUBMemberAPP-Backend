83d8af093950c970bf74a26be9fbb36452800551
Skyb0rg <github@skyborg.de>
2015-10-26 13:06:08 +0100

    [Finishes #106588178]

--- memberapp-refresh.php
+++ memberapp-refresh.php
@@ -9,11 +9,9 @@ use Parse\ParseUser;
 
 ParseClient::initialize('rG0fHSY1NGztjMC3k6DJgsOR6FSjGiawXO9ss2kb', 'rd9Z65vMXxT8bhOqcS9UUGWizzaJvymuG2U7KN9e', 'biJ8p7ZfQoE7QVNyeqkini8ZXH4mP6QTFoUgx9AU');
 
-
-
-//$testObject = ParseObject::create("TestObject");
-//$testObject->set("foo", "bar");
-//$testObject->save();
+	//Benutzerliste auslesen
+    $queryUserList = ParseUser::query();
+    $resultsuserList = $queryUserList->find();
 
 $query = new ParseQuery("ChatMsgs");
 //$query->equalTo("Benutzer", "Jan");
@@ -24,21 +22,43 @@ for ($i = 0; $i < count($results); $i++) {
   $object = $results[$i];
   //echo print_r($object->get("Benutzer")) . "<br>";
   //User
+        $chatusername = $object->get("Benutzer");
+			for ($j = 0; $j < count($resultsuserList); $j++) {
+           $objectUserList = $resultsuserList[$j];
+            //echo print_r($chatusername->getObjectId()) . " - username<br>";
+            //echo print_r($objectUserList->getObjectId()) . " - Userlistvergleich<br>";
+            if($chatusername->getObjectId() == $objectUserList->getObjectId()) {
+                //echo "EQUAL !<br><br>";
+				$objectFindUserList = $objectUserList;
+            }else echo "";
+		
+		//echo "Chatbenutzername: " . print_r($objectFindUserList->get('username')) . "<br>";
+		//$objectuser = $resultsuser[0];
+        //echo "Chatbenutzer: " . print_r($objectuser->get('username')) . "<br>";
+        }
+  echo $objectFindUserList->get('username') . ": " . $object->get('content') . "<br>";
+
+}
+
+        
+
+?>
+
+// Alte for Schleife
+for ($i = 0; $i < count($results); $i++) {
+  $object = $results[$i];
+  //echo print_r($object->get("Benutzer")) . "<br>";
+  //User
         $queryUser = ParseUser::query();
         $chatusername = $object->get("Benutzer");
         $queryUser->equalTo("objectId", $chatusername->getObjectId());
         $resultsuser = $queryUser->find();
         $objectuser = $resultsuser[0];
-        //echo "Chatbenutzer: " . print_r($objectuser->get('username')) . "<br>";
+        echo "Chatbenutzer: " . print_r($objectuser->get('username')) . "<br>";
 
   echo $objectuser->get('username') . ": " . $object->get('content') . "<br>";
+
 }
-
-
-
-?>
-
-
 
 public function findUser($email)
 {
