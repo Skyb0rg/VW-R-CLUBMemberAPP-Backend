d6a61899ff3e5946c8b321685d80ed35b45739ff
Skyb0rg <github@skyborg.de>
2015-10-26 21:24:41 +0100

    Cleanup web chat GIT: [Finishes #106638382]

--- memberapp-refresh.php
+++ memberapp-refresh.php
@@ -1,4 +1,5 @@
 <?php
+require 'pass-include.php';
 
 require 'vendor/autoload.php';
 
@@ -7,41 +8,28 @@ use Parse\ParseObject;
 use Parse\ParseQuery;
 use Parse\ParseUser;
 
-ParseClient::initialize('rG0fHSY1NGztjMC3k6DJgsOR6FSjGiawXO9ss2kb', 'rd9Z65vMXxT8bhOqcS9UUGWizzaJvymuG2U7KN9e', 'biJ8p7ZfQoE7QVNyeqkini8ZXH4mP6QTFoUgx9AU');
+ParseClient::initialize('$Parse1', '$Parse2', '$Parse3');
 
-	//Benutzerliste auslesen
+	//UserList pre-read
     $queryUserList = ParseUser::query();
     $resultsuserList = $queryUserList->find();
 
 $query = new ParseQuery("ChatMsgs");
-//$query->equalTo("Benutzer", "Jan");
 $results = $query->find();
 echo "Empfange " . count($results) . " Chatnachrichten.<br>";
 
 for ($i = 0; $i < count($results); $i++) {
   $object = $results[$i];
-  //echo print_r($object->get("Benutzer")) . "<br>";
-  //User
+  //User compare
         $chatusername = $object->get("Benutzer");
 			for ($j = 0; $j < count($resultsuserList); $j++) {
            $objectUserList = $resultsuserList[$j];
-            //echo print_r($chatusername->getObjectId()) . " - username<br>";
-            //echo print_r($objectUserList->getObjectId()) . " - Userlistvergleich<br>";
             if($chatusername->getObjectId() == $objectUserList->getObjectId()) {
-                //echo "EQUAL !<br><br>";
 				$objectFindUserList = $objectUserList;
             }else echo "";
-		
-		//echo "Chatbenutzername: " . print_r($objectFindUserList->get('username')) . "<br>";
-		//$objectuser = $resultsuser[0];
-        //echo "Chatbenutzer: " . print_r($objectuser->get('username')) . "<br>";
         }
   echo $objectFindUserList->get('username') . ": " . $object->get('content') . "<br>";
-
-}
-
-        
-
+}      
 ?>
 
 // Alte for Schleife
